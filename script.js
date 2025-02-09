d4113659e7bf895374cd81e4353ef8575462220d
Volodymyr Bobanych <v.i.bobanych@gmail.com>
2025-02-09 18:28:14 +0200

    💁🏻

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hi')
+function formatMessage(message, maxLength) {
+    let result;
+    // Change code below this line
+  if (message.length <= maxLength) {
+    result = message
+  } else {
+    result = message.slice(0,maxLength)+'...'
+  }
+    /// Change code above this line
+    return result;
+  }
+console.log(  formatMessage("Vestibulum facilisis purus nec", 20))
