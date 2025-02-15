1bff133967c3997b266d027405a1bf40c90dec87
Volodymyr Bobanych <v.i.bobanych@gmail.com>
2025-02-15 23:06:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,87 @@
-const atTheOldToad = {
-    potions: [
-      { name: "Speed potion", price: 460 },
-      { name: "Dragon breath", price: 780 },
-      { name: "Stone skin", price: 520 },
-    ],
-    // Change code below this line
-    getPotions() {
-      return this.potions;
-    },
+// Change code below this line
+const getUserNames = users => {
+  return  users.map(el => el.name)
+};
+// Change code above this line
 
-    addPotion(newPotion) {
 
-        for (let potion of this.potions) {
-            if(potion.name === newPotion.name) {
-            return `Error! Potion ${newPotion.name} is already in your inventory!`
-        }
-        
 
-      }
-      this.potions.push(newPotion)
-    },
-    removePotion(potionName) {
-      
-      const potionIndex = this.potions.indexOf(potionName);
-  
-      
-      if (potionIndex === -1) {
-        return `Potion ${potionName} is not in inventory!`;
-      }
-  
-      this.potions.splice(potionIndex, 1);
-    },
-    updatePotionName(oldName, newName) {
-      const potionIndex = this.potions.indexOf(oldName);
-  
-      if (potionIndex === -1) {
-        return `Potion ${oldName} is not in inventory!`;
-      }
-  
-      this.potions.splice(potionIndex, 1, newName);
-    },
-    // Change code above this line
-  };
-
-console.log(atTheOldToad.potions)
+console.log(getUserNames([
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    skills: ["ipsum", "lorem"],
+    gender: "male",
+    age: 37,
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+    gender: "female",
+    age: 34,
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+    gender: "male",
+    age: 24,
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    skills: ["adipisicing", "irure", "velit"],
+    gender: "female",
+    age: 21,
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    skills: ["ex", "culpa", "nostrud"],
+    gender: "male",
+    age: 27,
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    skills: ["non", "amet", "ipsum"],
+    gender: "male",
+    age: 38,
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    skills: ["lorem", "veniam", "culpa"],
+    gender: "female",
+    age: 39,
+  },
+]))
